b77931153346259a23b1af3dae04057f5641d072
Skyshik <57954642+Skyshik@users.noreply.github.com>
2019-11-19 10:48:54 -0600

    Add files via upload

---
+++ cadena.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	char cadena[200];
+	int indice;
+	int contador = 0;
+	int leyoLetras = 0;
+
+	printf("Ingrese la cadena de caracteres: ");
+	gets(cadena);
+
+	for(indice=0; indice<strlen(cadena); indice++)
+	{
+		if(cadena[indice]==' ')
+		{
+			if(leyoLetras==1)
+			{
+				if(cadena[indice+1]!=' ')
+					contador++;
+			}
+		}
+		else
+			leyoLetras=1;
+	}
+
+	if(cadena[indice-1]!=' ')
+		contador++;
+
+	printf("\nFrase: %s", cadena);
+	printf("\nNumero de palabras: %i", contador);
+	printf("\nPalabra\tCaracteres ");
+	return 0;
+}
